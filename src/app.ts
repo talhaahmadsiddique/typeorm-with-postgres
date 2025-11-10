25aa6d90b72962bef5968cfb7b18770c6336ef82
talhaahmadsiddique <talha.siddique.dev@gmail.com>
2025-11-10 21:41:21 +0500

    connect the db

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
+import 'reflect-metadata';
 import express from 'express';
+import { DataSource } from 'typeorm';
 
 const app = express();
 
@@ -7,11 +9,27 @@ app.use(express.json());
 const port = 8000;
 
 app.get('/', (req, res) => {
-    res.send('Hello From Express!!');
+  res.send('Hello From Express!!');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'typeorm_db',
 });
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
 
 export default app;
